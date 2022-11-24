e77dc30be9380ebc3a9f014639dc1fc7d7e65b96
yann cabrel <yannfonkoue@gmail.com>
2022-11-24 19:01:24 +0100

    creation d'un nouveau project react

--- src/App.js
+++ src/App.js
@@ -1,99 +1,49 @@
-import React, { useState } from "react";
+import React from "react";
 import CardItem from "./Components/CardItem";
 import Card from "./Components/Card/Card";
-import { FcPrevious } from "react-icons/fc";
-import { FcNext } from "react-icons/fc";
 
-const Persons = [
+const textTable = [
   {
     id: 1,
-    name: "Susan pedro",
-    profession: "Web Developper",
-    description:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
-    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    title: "Do I have to allow the use of cookes?",
+    text: "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
   },
   {
     id: 2,
-    name: "Peter Jon",
-    profession: "Intern",
-    description:
-      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
-    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    title: "How do I change my My Page password?",
+    text: "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.",
   },
   {
     id: 3,
-    name: "Bill Anderson",
-    profession: "The boss",
-    description:
-      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic.",
-    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    title: "What is BankID?",
+    text: "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
   },
   {
     id: 4,
-    name: "Anna Johnson",
-    profession: "Web Designer",
-    description:
-      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
-    img: "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    title: "Whose birth number can I use?",
+    text: "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
   },
 ];
 
 function App() {
-  document.getElementById("body").classList = "w-full h-full bg-blue-100";
-  let [people, setPeople] = useState(0);
-
-  const ramdomPersonHandler = () => {
-    setPeople((people = Math.floor(Math.random() * 4 + 0)));
-  };
-
-  const nextPeopleHandler = () => {
-    setPeople(() => {
-      if (people === Persons.length) {
-        return (people = 0);
-      } else {
-        return people++;
-      }
-    });
-  };
-
-  const prevPeopleHandler = () => {
-    if (people <= 0) {
-      setPeople(Persons.length - 1);
-    } else {
-      setPeople((people = people - 1));
-    }
-  };
-
   return (
     <React.Fragment>
-      <div className="flex flex-col items-center mt-8">
-        <h1 className="text-5xl text-gray-600">Our Riview</h1>
-        <div className="border-b-4 border-blue-400 w-28 mt-2"></div>
+      <div className="container">
+        <Card className="bg-white  text-gray-700 rounded-sm  mx-lg-1 p-4 mt-8  ">
+          <div className="row">
+            <div className="col-lg-4 col-md-4 col-12">
+              <h1 className="text-2xl my-lg-5">
+                Questions And Answers About Login
+              </h1>
+            </div>
+            <div className="col-lg-8 col-12 col-md-8 px-lg-3">
+              {textTable.map((text) => (
+                <CardItem title={text.title} text={text.text} />
+              ))}
+            </div>
+          </div>
+        </Card>
       </div>
-
-      <Card className="bg-white mx-96 rounded-lg  shadow-lg my-8 p-8 flex flex-col items-center">
-        <CardItem
-          name={Persons[people].name}
-          img={Persons[people].img}
-          profession={Persons[people].profession}
-          description={Persons[people].description}
-        />
-        <div className="flex flex-row w-20 justify-around  mb-1">
-          <button onClick={prevPeopleHandler} className="p-2">
-            <FcPrevious size={30} />
-          </button>
-          <button onClick={nextPeopleHandler} className="p-2">
-            <FcNext size={30} />
-          </button>
-        </div>
-        <button
-          onClick={ramdomPersonHandler}
-          className="bg-blue-400 text-white p-2 rounded-lg"
-        >
-          Surprise Me
-        </button>
-      </Card>
     </React.Fragment>
   );
 }
--- src/Components/CardItem.js
+++ src/Components/CardItem.js
@@ -1,28 +1,35 @@
-import React, { Fragment } from "react";
-import { BsFillChatQuoteFill } from "react-icons/bs";
+import React, { Fragment, useState } from "react";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { AiFillMinusCircle } from "react-icons/ai";
 
-function CardItem({ img, name, profession, description }) {
+function CardItem({ title, text }) {
+  let [isClick, setIsClick] = useState(false);
+  let button;
+  if (isClick === false) {
+    button = <AiFillPlusCircle size={35} />;
+  } else {
+    button = <AiFillMinusCircle size={35} />;
+  }
+  const clickHandler = () => {
+    setIsClick(!isClick);
+  };
   return (
     <Fragment>
-      <div className="flex flex-col items-center w-full h-full">
-        <div className="rounded-full border-r-8 border-blue-400 w-46 h-36 bg-blue-400 relative">
-          <img
-            src={img}
-            alt="person"
-            className="rounded-full w-36 h-36 object-cover"
-          />
-          <BsFillChatQuoteFill
-            size={35}
-            className="text-blue-400 absolute top-0"
-          />
+      <div className="shadow rounded-lg px-lg-2 py-lg-3 mt-4 container ">
+        <div className="row align-items-center">
+          <div className="col-8 col-lg-8 col-md-8 py-2">
+            <h1 className="font-bold text-gray-900 pr-2">{title}</h1>
+          </div>
+          <div className="col-4 col-lg-4 col-md-4 py-2">
+            <button
+              onClick={clickHandler}
+              className="text-gray-400 hover:text-gray-600"
+            >
+              {button}
+            </button>
+          </div>
         </div>
-        <div className="text-center mt-3">
-          <h1 className="text-gray-800 text-md">{name}</h1>
-          <h2 className="text-blue-400 uppercase text-md font-light mt-2">
-            {profession}
-          </h2>
-          <p className="text-sm text-gray-500 my-1">{description}</p>
-        </div>
+        <div className={`py-2 text-sm  ${!isClick && "hidden"}`}>{text}</div>
       </div>
     </Fragment>
   );
